9cd79be320419762dc21ef638c037a4081d96367
GitHub Action <action@github.com>
2025-12-17 12:38:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/thisisdun998/findfriend/services/MyProjectService.kt
+++ src/main/kotlin/com/github/thisisdun998/findfriend/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.thisisdun998.findfriend.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thisisdun998.findfriend.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
